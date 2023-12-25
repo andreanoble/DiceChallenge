fc32a0212a4dba65693b8bca5d7e11743c400503
andreanoble <anoble.sw@gmail.com>
2023-12-25 01:09:23 -0500

    dice

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,34 @@
+//random number 1 to 6 inclusive for player 1
+let randomNumber1 = Math.floor(Math.random()*6)+1;
+
+//player 1 change dice image
+let srcImage1 = `images/dice${randomNumber1}.png`;
+document.querySelector(".dice .img1").setAttribute("src", srcImage1);
+
+//random number 1 to 6 inclusive for player 2
+let randomNumber2 = Math.floor(Math.random()*6)+1;
+
+//player 2 change dice image
+let srcImage2 = `images/dice${randomNumber2}.png`;
+document.querySelector(".dice .img2").setAttribute("src", srcImage2);
+
+//Display results, Player 1 Wins!,  Player 2 Wins! or Draw
+let message = "";
+if(randomNumber1 === randomNumber2){
+    message = "Draw!";
+}else if(randomNumber1 > randomNumber2){
+    message = "Player 1 Wins!"
+} else {
+    message = "Player 2 Wins!";
+}
+//change h1 message
+document.querySelector("h1").textContent = message;
+
+
+
+console.log(document.querySelector(".dice .img1"));
+console.log(randomNumber1);
+console.log(srcImage1);
+console.log(srcImage2);
+console.log(document.querySelector("h1").textContent);
+console.log(message);
